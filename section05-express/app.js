9add8c0ec8c1ac2b26e189dd2f68e04d06dad21e
Oscar Chan <oscarchan@appicidea.com>
2021-10-05 10:03:30 +0800

    feat: add static style

--- section05-express/app.js
+++ section05-express/app.js
@@ -10,6 +10,7 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.use("/", (req, res, next) => {
   console.log("This always runs");
