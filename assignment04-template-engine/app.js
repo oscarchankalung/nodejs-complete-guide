e6373c93045470843b4dbbcae6e60b3c8a618532
oscarchankalung <oscarchankalung@gmail.com>
2022-05-13 14:32:07 +0800

    feat: redo assignment04

--- assignment04-template-engine/app.js
+++ assignment04-template-engine/app.js
@@ -1,12 +1,17 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const { engine } = require("express-handlebars");
 
 const app = express();
+const rootDir = require("./utils/path");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// middlewares
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// engine
 app.engine(
   "hbs",
   engine({
@@ -15,11 +20,36 @@ app.engine(
     extname: "hbs",
   })
 );
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
 app.set("views", "views");
 
-const routes = require("./routes/index");
+// data
+const users = [];
 
-app.use(routes);
+// routes
+app.get("/", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, "views", "index.html"));
+  res.render("index", {
+    pageTitle: "Add User",
+    path: "/",
+    activeIndex: true,
+  });
+});
+
+app.get("/users", (req, res, next) => {
+  // res.sendFile(path.join(rootDir, "views", "users.html"));
+  res.render("users", {
+    pageTitle: "Users List",
+    path: "/users",
+    activeUser: true,
+    users: users,
+    hasUsers: users.length > 0,
+  });
+});
+
+app.post("/add-user", (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect("/users");
+});
 
 app.listen(3000);
